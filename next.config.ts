4bf32a49d7d11a77fc03347d88b8a30608c40837
utulb <39941967+utulb@users.noreply.github.com>
2025-05-28 00:32:20 +0300

    export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+// next.config.js
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // This tells Next.js to export the app as a static site
+  trailingSlash: true, // Optional: Adds trailing slashes to your URLs
 };
 
 export default nextConfig;
